8ac04b03d79e2c11ba4fcaf200b06c2f4d6be4cb
StrayStrider <StrayStrider@users.noreply.github.com>
2024-01-27 18:46:32 -0700

    added decimal places to add

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -10,7 +10,7 @@ function add(number1, number2) {
 function addNumbers() {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
-    document.querySelector('#sum').value = add(addNumber1, addNumber2)
+    document.querySelector('#sum').value = add(addNumber1, addNumber2).toFixed(2);
 };
 
 
